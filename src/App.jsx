797219bd685da6fd87f750bfc97dad8798fffb5f
shelly <shellyrathi36@gmail.com>
2025-07-28 15:30:57 +0530

    third_commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import About from "./components/About/About";
 import MyWork from "./components/MyWork/MyWork";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
+import { Analytics } from "@vercel/analytics/next";
 const App = () => {
   return (
     <div>
@@ -14,6 +15,7 @@ const App = () => {
       <MyWork />
       <Contact />
       <Footer />
+      <Analytics />
     </div>
   );
 };
